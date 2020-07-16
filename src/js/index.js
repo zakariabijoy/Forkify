cf27b7ad1066bb6debaa4ac48320e8c2c127650b
zakariabijoy <zakaria@vonome.com>
2020-07-16 14:13:45 +0600

    project is setting up using node.js, npm, webpack, babel

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,5 @@
 // Global app controller
+import x from './test'
+
+const c = 23;
+console.log(`imported ${x} from anthor module, variable is ${c}`);
---
+++ src/js/test.js
@@ -0,0 +1,2 @@
+console.log('imported module 2');
+export default 23
