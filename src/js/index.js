cda58fdd5d5cdaafffae6967cc2389e029a20b64
zakariabijoy <zakaria@vonome.com>
2020-07-19 16:37:02 +0600

    Building the search controller

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,33 @@
 
 import Search from "./models/Search";
 
-const search = new Search('pizza');
-console.log(search);
+/** Global state of the app
+ * - Search Object
+ * - Current recipe Object
+ * shopping list object
+ * liked recipe 
+ * */ 
+const state = {};
 
-search.getResults();
+const controlSearch = async () =>{
+    // 1) Get query from view
+    const query = 'pizza'; // to do
+
+    if(query){
+        // 2) New search object and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+
+        // 4) search for recipes
+        await state.search.getResults();
+
+        // 5) render results on UI
+        console.log(state.search.result);
+    }
+};
+
+document.querySelector('.search').addEventListener('submit', e =>{
+    e.preventDefault();
+    controlSearch();
+});
