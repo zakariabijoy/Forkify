a03716addcd53cde7cd922bc113faaf7e619e74f
zakariabijoy <zakaria@vonome.com>
2020-07-19 18:03:10 +0600

    Building the search view part 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,8 @@
 // Global app controller
 
 import Search from "./models/Search";
+import * as searchView from './views/searchView';
+import { elements } from "./views/base";
 
 /** Global state of the app
  * - Search Object
@@ -12,23 +14,24 @@ const state = {};
 
 const controlSearch = async () =>{
     // 1) Get query from view
-    const query = 'pizza'; // to do
+    const query = searchView.getInput();
 
     if(query){
         // 2) New search object and add to state
         state.search = new Search(query);
 
         // 3) Prepare UI for results
-
+        searchView.clearInput();
+        searchView.clearResult();
         // 4) search for recipes
         await state.search.getResults();
 
         // 5) render results on UI
-        console.log(state.search.result);
+        searchView.renderResults(state.search.result);
     }
 };
 
-document.querySelector('.search').addEventListener('submit', e =>{
+elements.searchForm.addEventListener('submit', e =>{
     e.preventDefault();
     controlSearch();
 });
