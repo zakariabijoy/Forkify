45d287a6ccd41328ab9a219a67c5c5e2825275ba
zakariabijoy <zakaria@vonome.com>
2020-07-23 21:53:55 +0600

    building the shopping list model

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,7 @@
 
 import Search from "./models/Search";
 import Recipe from "./models/Recipe";
+import List from "./models/List";
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from "./views/base";
@@ -119,5 +120,5 @@ elements.recipe.addEventListener('click', e=>{
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
     }
-    console.log(state.recipe);
+   
 });
